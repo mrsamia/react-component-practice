d059ae1ad57f62fb62d586b66fcb0d3841668be0
mrsamia <15203060@iubat.edu>
2021-12-08 21:17:02 +0600

    SRM | feature

--- src/App.js
+++ src/App.js
@@ -48,51 +48,38 @@ function App() {
       </div>
 
       <div className="container">
-        <div>
-          <Feature
-            h3="Give and get feedback, effortlessly"
-            p="The feedback process may feel a bit daunting at first. This is why we built
+        <Feature
+          h3="Give and get feedback, effortlessly"
+          p="The feedback process may feel a bit daunting at first. This is why we built
         Skills in Worklife. Skills are the most intuitive way to document and organize the competencies that are important to you, your team, and your company.
         Once set up, you can ask for feedback on them from coworkers in just a few clicks. So, you never have to worry about what to say or write the next time you go looking for feedback."
-            img={Mim}
-            isLeft={true}
-          />
-        </div>
-        <div>
-          <div className="row">
-            <div className="col-6">
-              <Feature
-                h3="Build a feedback culture"
-                p="Sharing feedback about yourself is a great way to normalize feedback and create psychological safety across your team, especially if you’re in a leadership role.
+          img={Mim}
+          isLeft={true}
+        />
+
+        <Feature
+          h3="Build a feedback culture"
+          p="Sharing feedback about yourself is a great way to normalize feedback and create psychological safety across your team, especially if you’re in a leadership role.
               While Worklife is private by default, we have also built capabilities to seamlessly share your feedback with your manager, team, or the entire company."
-                img={require("./image/feature_1.png").default}
-                isLeft={false}
-              />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-6">
-              <Feature
-                h3="Build a feedback culture"
-                p="Sharing feedback about yourself is a great way to normalize feedback and create psychological safety across your team, especially if you’re in a leadership role.
+          img={require("./image/feature_1.png").default}
+          isLeft={false}
+        />
+
+        <Feature
+          h3="Build a feedback culture"
+          p="Sharing feedback about yourself is a great way to normalize feedback and create psychological safety across your team, especially if you’re in a leadership role.
               While Worklife is private by default, we have also built capabilities to seamlessly share your feedback with your manager, team, or the entire company."
-                img={require("./image/feature_1.png").default}
-                isLeft={true}
-              />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-6">
-              <Feature
-                h3="Build a feedback culture"
-                p="Sharing feedback about yourself is a great way to normalize feedback and create psychological safety across your team, especially if you’re in a leadership role.
+          img={require("./image/feature_1.png").default}
+          isLeft={true}
+        />
+
+        <Feature
+          h3="Build a feedback culture"
+          p="Sharing feedback about yourself is a great way to normalize feedback and create psychological safety across your team, especially if you’re in a leadership role.
               While Worklife is private by default, we have also built capabilities to seamlessly share your feedback with your manager, team, or the entire company."
-                img={require("./image/feature_1.png").default}
-                isLeft={false}
-              />
-            </div>
-          </div>
-        </div>
+          img={require("./image/feature_1.png").default}
+          isLeft={false}
+        />
       </div>
     </div>
   );
--- src/component/Feature.js
+++ src/component/Feature.js
@@ -2,35 +2,34 @@ import "../style/feature.css";
 
 function Feature({ h3 = "", p = "", img = "", isLeft = false }) {
   return (
-    <div>
-      <div className="row d-flex align-items-center">
-        {isLeft == true && (
-          <div className="col-6 pt-5">
-            <img className="img_size" src={img} />
-          </div>
-        )}
-
-        <div className="col-6 pt-5 ">
-          <div>
-            <h3 className="text-center">{h3}</h3>
-          </div>
-          <div>
-            <p>{p}</p>
-          </div>
+    <div className="row d-flex align-items-center">
+      {isLeft == true && (
+        <div className="col-6 pt-5">
+          <img className="img_size" src={img} />
         </div>
+      )}
 
-        {isLeft == false && (
-          <div className="col-6 pt-5">
-            <img src={img} />
-          </div>
-        )}
+      <div className="col-6 pt-5 ">
+        <div>
+          <h3 className="text-center">{h3}</h3>
+        </div>
+        <div>
+          <p>{p}</p>
+        </div>
       </div>
 
-      {/* {isLeft && <img src={img} />}
-      <h3>{h3}</h3>
-      <p>{p}</p>
-      {!isLeft && <img src={img} />} */}
+      {isLeft == false && (
+        <div className="col-6 pt-5">
+          <img className="img_size" src={img} />
+        </div>
+      )}
     </div>
   );
 }
 export default Feature;
+{
+  /* {isLeft && <img src={img} />}
+      <h3>{h3}</h3>
+      <p>{p}</p>
+      {!isLeft && <img src={img} />} */
+}
